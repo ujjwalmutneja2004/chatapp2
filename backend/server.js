d15fc4601d7320e215149081beaadac4083ab500
ujjwalmutneja2004 <ujjwalmutneja2004@gmail.com>
2025-07-21 03:14:03 +0530

    Origin changed

--- backend/server.js
+++ backend/server.js
@@ -46,7 +46,7 @@ const server=app.listen(PORT, console.log(`Server started on port ${PORT}`));
 const io=require('socket.io')(server,{
     pingTimeout:60000,
     cors:{
-        origin:"http://localhost:3000",
+        origin:"https://chatapp2-8nrb.vercel.app",
     },
 })
 
